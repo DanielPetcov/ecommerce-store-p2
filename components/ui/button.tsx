ef6e5cdfcbfd0a9ce7c59313718135cb4dbe1c78
daniel petcov <danielpetcov.work@gmail.com>
2025-09-12 17:01:41 +0300

    done structuring the footer, navbar and also creating a custom made button

---
+++ components/ui/button.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@/lib/utils";
+import { ComponentPropsWithRef } from "react";
+
+export default function Button({
+  className,
+  children,
+  disabled,
+  type = "button",
+  ref,
+  ...props
+}: ComponentPropsWithRef<"button">) {
+  return (
+    <button
+      className={cn(
+        `w-auto 
+        rounded-full 
+        bg-black 
+        border-transparent 
+        px-5 
+        py-3 
+        disabled:cursor-not-allowed 
+        disabled:opacity-50 
+        text-white 
+        font-semibold 
+        hover:opacity-75 
+        transition`,
+        className
+      )}
+      ref={ref}
+    >
+      {children}
+    </button>
+  );
+}

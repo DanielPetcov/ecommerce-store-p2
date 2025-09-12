192c54e800d16d50de4708914ad5883ef350f17c
daniel petcov <danielpetcov.work@gmail.com>
2025-09-12 17:13:37 +0300

    done making the billboard component also made the types for product, color, size and images

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,3 +1,16 @@
-export default function HomePage() {
-  return <div>hello</div>;
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+import getBillboard from "@/actions/get-billboard";
+
+export const reavlidate = 0;
+
+export default async function HomePage() {
+  const billboard = await getBillboard("");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10 ">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 }
